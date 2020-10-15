4453311ec08d9857adab156252cfd3115ef63ada
xiaomo1q <55676359+xiaomo1q@users.noreply.github.com>
2020-10-15 13:08:45 +0800

    Update config.prod.js

--- UMIProject-AppleH5/config/config.prod.js
+++ UMIProject-AppleH5/config/config.prod.js
@@ -2,8 +2,8 @@ export default {
     hash: true,
     /**服务器地址*/
     define: {
-        'process.env.apiServerUrl': 'http://babistep.com',
-        'process.env.host': 'http://babistep.com',
+        'process.env.apiServerUrl': 'http://juuuce.com',
+        'process.env.host': 'http://juuuce.com',
     },
 
 }
