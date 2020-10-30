69216e5a1fabb38abcba9cad299cc7fe8474b286
pzr <2604011581@qq.com>
2020-10-31 00:07:19 +0800

    mock

--- UMIProject-AppleH5/src/constants/server-apis.js
+++ UMIProject-AppleH5/src/constants/server-apis.js
@@ -5,9 +5,9 @@
 /**
  * 服务器地址(开发)
  */
-const BASE_HOST_URL = process.env.apiServerUrl;
+// const BASE_HOST_URL = process.env.apiServerUrl;
 // const BASE_HOST_URL = "https://juuuce.com";
-// const BASE_HOST_URL = "https://project-sc9.github.io/Apple";
+const BASE_HOST_URL = "https://project-sc9.github.io/Apple";
 
 /**
  * 服务器路径
@@ -17,22 +17,23 @@ const API_PATH = '/juuuce';
 /**
  * 任务栏目标获取
  */
-export const GET_TASK_LIST = BASE_HOST_URL + API_PATH + '/task/get.json';
+// export const GET_TASK_LIST = BASE_HOST_URL + API_PATH + '/task/get.json';
+export const GET_TASK_LIST = 'https://www.fastmock.site/mock/7ab848fc865a67251ec32ab577b23d78/api/task/get';
 
 /**
  * 获取图片列表
  */
-export const GAME_LIST = BASE_HOST_URL + API_PATH + '/pic/list.json';
+// export const GAME_LIST = BASE_HOST_URL + API_PATH + '/pic/list.json';
+export const GAME_LIST = 'https://www.fastmock.site/mock/7ab848fc865a67251ec32ab577b23d78/api/pic/list';
 
 
 /**
  * 用户日志采集【异步】
  */
-export const LOG_SAVE = BASE_HOST_URL + API_PATH + '/log/save.json';
-
+// export const LOG_SAVE = BASE_HOST_URL + API_PATH + '/log/save.json';
 
 
 /**
  * 每天任务上限值获取
  */
-export const TASK_LIMIT = BASE_HOST_URL + API_PATH + '/task/limit.json';
+// export const TASK_LIMIT = BASE_HOST_URL + API_PATH + '/task/limit.json';
--- UMIProject-AppleH5/src/services/index.js
+++ UMIProject-AppleH5/src/services/index.js
@@ -3,8 +3,8 @@ import { GETRequest, POSTRequest } from 'utils/net-requests';
 import {
     GET_TASK_LIST,
     GAME_LIST,
-    LOG_SAVE,
-    TASK_LIMIT
+    // LOG_SAVE,
+    // TASK_LIMIT
 } from 'constants/server-apis';
 
 /**
@@ -13,6 +13,9 @@ import {
 export const GetTaskList = (uid, tid, type) => {
     return GETRequest(GET_TASK_LIST, { uid, tid, type });
 }
+// export const GetTaskList = (uid, tid, type) => {
+//     return GETRequest("/task/get", { uid, tid, type });
+// }
 
 
 /**
@@ -21,17 +24,25 @@ export const GetTaskList = (uid, tid, type) => {
 export const GameList = (uid, tid, total) => {
     return GETRequest(GAME_LIST, { uid, tid, total });
 }
+// export const GameList = (uid, tid, total) => {
+//     return GETRequest('/pic/list', { uid, tid, total });
+// }
 
 /**
  * 用户日志采集【异步】
  */
-export const LogSave = (uid, tid, time, log,) => {
-    return GETRequest(LOG_SAVE, { uid, tid, time, log, });
-}
+// export const LogSave = (uid, tid, time, log,) => {
+//     return GETRequest(LOG_SAVE, { uid, tid, time, log, });
+// }
 
 /**
  * 每天任务上限值获取
  */
-export const TaskLimit = (uid) => {
-    return GETRequest(TASK_LIMIT, { uid });
-}
+// export const TaskLimit = (uid) => {
+//     return GETRequest(TASK_LIMIT, { uid });
+// }
+
+
+// export const test = () => {
+//     return GETRequest('/api/test.html');
+// }
